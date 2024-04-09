cfd48c2d75dd57925916f7170ced94a64a75cfe6
Leonardo Mendez <leo.mendez.9@outlook.com>
2024-04-09 09:19:33 -0500

    Add project

---
+++ MagicTheGathering/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MagicTheGathering
+//
+//  Created by Leonardo Mendez on 9/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MagicTheGathering/MagicTheGatheringApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicTheGatheringApp.swift
+//  MagicTheGathering
+//
+//  Created by Leonardo Mendez on 9/04/24.
+//
+
+import SwiftUI
+
+@main
+struct MagicTheGatheringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
